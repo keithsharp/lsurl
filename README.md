7fe7f4241f297b86a3e3761ef5c39012415a01c1
Keith Sharp <kms@passback.co.uk>
2022-06-27 12:59:38 +0100

    Implemented URL functionality. Added README.md file. Added LICENSE.txt (AGPL 3.0) file.

---
+++ README.md
@@ -0,0 +1,25 @@
+# `lsurl`
+Print paths (files or directories) as `file://` URLs.
+
+Usage:
+```bash
+$ lsurl
+file:///Users/kms/
+$ lsurl .
+file:///Users/kms/
+$ lsurl /Users
+file:///Users/
+$ lsurl /Users/*
+file:///Users/Guest/
+file:///Users/Shared/
+file:///Users/kms/
+```
+
+# Copyright and License
+Copyright 2022, Keith Sharp, kms@passback.co.uk.
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License along with this program.  If not, see <https://www.gnu.org/licenses/>.
--- lsurl/main.swift
+++ lsurl/main.swift
@@ -5,7 +5,52 @@
 //  Created by Keith Sharp on 27/06/2022.
 //
 
+import ArgumentParser
 import Foundation
+import System
 
-print("Hello, World!")
+enum UrlError: Error {
+    case noSuchFile(path: String)
+    case urlCreateFailure(path: String)
+}
 
+extension UrlError: LocalizedError {
+    public var errorDescription: String? {
+        switch self {
+        case let .noSuchFile(path):
+            return NSLocalizedString(
+                "No such file or directory: \(path)",
+                comment: "No such file or directory: "
+            )
+        case let .urlCreateFailure(path):
+            return NSLocalizedString(
+                "Could not create URL for path: \(path)",
+                comment: "URL Creation failed"
+            )
+        }
+    }
+}
+
+struct LsUrl: ParsableCommand {
+    
+    @Argument var paths: [String] = ["."]
+    
+    func run() {
+        let fm = FileManager.default
+        
+        let _ = paths.map { path in
+            if fm.fileExists(atPath: path) {
+                if let url = URL(FilePath(path)) {
+                    print(url.absoluteString)
+                } else {
+                    LsUrl.exit(withError: UrlError.urlCreateFailure(path: path))
+                }
+            } else {
+                LsUrl.exit(withError: UrlError.noSuchFile(path: path))
+            }
+        }
+    }
+}
+
+LsUrl.main()
+exit(EXIT_SUCCESS)
